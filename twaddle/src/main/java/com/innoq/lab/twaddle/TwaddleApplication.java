a8fd03eee494a82feadbe0d55ddcfc4f4a2870e7
ollegg <35693171+ollegg@users.noreply.github.com>
2018-01-23 10:17:04 +0100

    Add files via upload

---
+++ twaddle/src/main/java/com/innoq/lab/twaddle/TwaddleApplication.java
@@ -0,0 +1,12 @@
+package com.innoq.lab.twaddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwaddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwaddleApplication.class, args);
+	}
+}
